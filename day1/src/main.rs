0f474c3a8125326bad4828f6ba4aac54e11461fb
Andreas Andersson <andreas@herrandersson.se>
2019-12-21 08:53:43 +0100

    Solve day1:1

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+use std::fs;
+
+fn get_fuel_reqs(mass: u32) -> u32 {
+    return (mass / 3) - 2;
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let filename = args.get(1).expect("Usage: day1 input-filename");
+
+    println!("Reading mass of modules from {}", filename);
+
+    let module_masses = fs::read_to_string(filename).expect("Failed to read file");
+
+    let mut total_fuel: u32 = 0;
+    for line in module_masses.lines() {
+        let mass = line.parse::<u32>().unwrap();
+        let fuel = get_fuel_reqs(mass);
+        total_fuel += fuel;
+
+        println!("{} {}", mass, fuel);
+    }
+
+    println!("Total fuel reqs: {}", total_fuel);
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test() {
+        assert_eq!(get_fuel_reqs(12), 2);
+        assert_eq!(get_fuel_reqs(14), 2);
+        assert_eq!(get_fuel_reqs(1969), 654);
+        assert_eq!(get_fuel_reqs(100756), 33583);
+    }
 }
